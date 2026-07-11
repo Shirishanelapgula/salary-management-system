4a32e4de6993d758d05770985ff2c9727b5eb253
Nelapogula Shirisha <nelapogula_shirisha@epam.com>
2026-07-11 17:53:02 +0530

    Add seed constants

---
+++ backend/src/constants/seed-data.ts
@@ -0,0 +1,33 @@
+const departments = [
+  "Engineering",
+  "Human Resources",
+  "Finance",
+  "Sales",
+  "Marketing",
+  "Operations",
+  "Customer Support",
+  "Legal",
+];
+
+const countries = [
+  { name: "India", currency: "INR" },
+  { name: "United States", currency: "USD" },
+  { name: "United Kingdom", currency: "GBP" },
+  { name: "Germany", currency: "EUR" },
+  { name: "Canada", currency: "CAD" },
+  { name: "Australia", currency: "AUD" },
+  { name: "Singapore", currency: "SGD" },
+  { name: "Japan", currency: "JPY" },
+];
+
+const designations = [
+  "Intern",
+  "Software Engineer",
+  "Senior Software Engineer",
+  "Lead Engineer",
+  "Engineering Manager",
+  "Director",
+  "Vice President",
+];
+
+module.exports = { departments, countries, designations };
